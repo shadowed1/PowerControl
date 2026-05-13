a85cfd19353ca94ab307a7f628d69c0473633d2c
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-05-13 00:37:46 -0400

    Update Download_PowerControl.sh

--- bin/Download_PowerControl.sh
+++ bin/Download_PowerControl.sh
@@ -7,19 +7,6 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo "                                                         ${BOLD}Features:${RESET}"
-echo "${CYAN}"
-echo "╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════╗"
-echo "║ Control CPU clockspeed in relation to temperature; enabling lower temperatures under and longer battery life. ║"
-echo "╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════╝"
-echo "${RESET}"
-echo ""
-echo ""
 rm -f ~/Install_PowerControl.sh 2>/dev/null
 curl -L https://raw.githubusercontent.com/shadowed1/PowerControl/main/bin/Install_PowerControl.sh -o ~/Install_PowerControl.sh
 chmod +x ~/Install_PowerControl.sh
